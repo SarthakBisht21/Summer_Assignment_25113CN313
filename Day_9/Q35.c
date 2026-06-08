55a38e4a8757ea41cb94bd2563db22ad3a1ba49c
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-06-08 20:01:05 +0530

    Day_9 uplodaded.

---
+++ Day_9/Q35.c
@@ -0,0 +1,21 @@
+// Write a program to Print repeated character pattern. 
+// A 
+// BB 
+// CCC 
+// DDDD 
+// EEEEE 
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 0; i < 5; i++) { 
+        for(j = 0; j <= i; j++) {
+            printf("%c", 'A' + i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
