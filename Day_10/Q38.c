844c3937312583e52a78d6a5cc43dbe0e778cb95
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-06-09 11:58:41 +0530

    Day_10 uplodaded.

--- Day_10/Q38.c
+++ Day_10/Q38.c
@@ -1,18 +1,18 @@
-// Write a program to Print star pyramid. 
-//     * 
-//    *** 
-//   ***** 
+// Write a program to Print reverse pyramid. 
+// ********* 
 //  ******* 
-// *********
+//   ***** 
+//    *** 
+//     * 
 
 #include<stdio.h>
 int main(){
 
-    for(int i=0;i<5;i++){
+    for(int i=5;i>0;i--){
         for(int j=(4-i);j>=0;j--){
             printf(" ");
         }
-        for(int j=0;j<(2*i)+1;j++){
+        for(int j=0;j<(2*i)-1;j++){
             printf("*");
         }
         printf("\n");
