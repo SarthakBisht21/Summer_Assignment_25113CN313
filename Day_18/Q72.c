81a5b98f35d03834123c74e2e9a51da98951f10b
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-06-17 21:55:13 +0530

    Day_18 uplodaded.

---
+++ Day_18/Q72.c
@@ -0,0 +1,23 @@
+// Q72 Write a program to Sort array in descending order
+#include <stdio.h>
+
+int main() {
+    int a[100], n, temp;
+
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(a[j] < a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) printf("%d ", a[i]);
+
+    return 0;
+}
