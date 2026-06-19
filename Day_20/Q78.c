5ea359845a53f5fd1d77a263f155e5328f6d5f36
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-06-19 21:42:23 +0530

    Day_20 uplodaded.

---
+++ Day_20/Q78.c
@@ -0,0 +1,26 @@
+// Q78 Write a program to Check symmetric matrix
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, flag = 1;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag) printf("Symmetric");
+    else printf("Not symmetric");
+
+    return 0;
+}
