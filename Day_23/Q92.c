fc918bc87ae8e8d26340f0e70fd13c8b9ae6e112
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-06-22 21:46:43 +0530

    Day_23 uplodaded.

---
+++ Day_23/Q92.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+
+    printf("Enter string: ");
+    gets(str);
+
+    for(int i = 0; str[i]; i++)
+        freq[str[i]]++;
+
+    int max = 0;
+    char ch;
+
+    for(int i = 0; i < 256; i++) {
+        if(freq[i] > max) {
+            max = freq[i];
+            ch = i;
+        }
+    }
+
+    printf("Maximum occurring character: %c", ch);
+    return 0;
+}
