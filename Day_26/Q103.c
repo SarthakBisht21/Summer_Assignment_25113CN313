ace6bbbff59104be92c68fba7e37b0a2d342ffa4
SarthakBisht21 <25113cn313@glbitm.ac.in>
2026-06-25 23:54:35 +0530

    Day_26 uplodaded.

---
+++ Day_26/Q103.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float balance = 1000, amount;
+
+    do {
+        printf("\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Balance: %.2f\n", balance);
+                break;
+
+            case 2:
+                printf("Enter amount to deposit: ");
+                scanf("%f", &amount);
+                balance += amount;
+                break;
+
+            case 3:
+                printf("Enter amount to withdraw: ");
+                scanf("%f", &amount);
+                if(amount <= balance)
+                    balance -= amount;
+                else
+                    printf("Insufficient balance\n");
+                break;
+
+            case 4:
+                printf("Thank you!\n");
+                break;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    } while(choice != 4);
+
+    return 0;
+}
